a880de0215856516bc60a134a246d3cee5743012
shivam15668 <shivamkumar15669@gmail.com>
2024-03-11 22:37:13 +0530

    how to run mongodb installation

---
+++ hacking/server/server.py
@@ -0,0 +1,13 @@
+# ips.txt contains ip ranges
+# masscan contains extracted ip
+# server folder will contain  mongodb
+# install mongodb community 
+# sudo systemctl start mongod
+# sudo systemctl stop mongod
+# sudo systemctl restart mongod
+# sudo systemctl enable mongod
+# sudo systemctl status mongod
+# sudo systemctl daemon-reload 
+# mongosh
+# sudo systemctl enable mongod.service # auto restart
+# mongodb compass download for gui 
