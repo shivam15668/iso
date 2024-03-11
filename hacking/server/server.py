de619343ef609147ffdba7e693f02717cc276a3d
shivam15668 <shivamkumar15669@gmail.com>
2024-03-12 00:09:58 +0530

    collections for mongodb

--- hacking/server/server.py
+++ hacking/server/server.py
@@ -16,3 +16,17 @@
 # pip install gunicorn
 # json module
 # re module 
+from flask import Flask, request, jsonify, Response , render_template_string
+import json
+from bson.regex import Regex
+import re
+from pymongo import MongoClient
+
+app = Flask(__name__)
+mongo_url = "mongodb://localhost:27017/"
+client = MongoClient(mongo_url)
+try:
+    db = client["scannerdb"]
+    collection = db["sslchecker"] # collections are in mongodb where we store all our information in json file 
+except Exception as e:
+    print(f"Error connection to MongoDB: {str(e)}")
